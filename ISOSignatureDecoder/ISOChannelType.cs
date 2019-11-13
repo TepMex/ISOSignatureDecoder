fe7cb3d4f4b149db75495b75201aa6978a391829
Semikolenov.MA <Semikolenov.MA@omsk.gazprom-neft.ru>
2019-11-13 08:46:07 +0600

    namespace changed

--- ISOSignatureDecoder/ISOChannelType.cs
+++ ISOSignatureDecoder/ISOChannelType.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SignatureSDKTest.ISOSignatureDecoder
+namespace ISOSignatureDecoder
 {
     public enum ISOChannelType : int
     {
--- ISOSignatureDecoder/ISOSignature.cs
+++ ISOSignatureDecoder/ISOSignature.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SignatureSDKTest.ISOSignatureDecoder
+namespace ISOSignatureDecoder
 {
     public class ISOSignature
     {
